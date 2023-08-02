53040573ed063c07da03a74522faf1984b7e4b46
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-08-02 19:48:02 +0200

    create bard clone with streamlit

--- sql.py
+++ sql.py
@@ -1,55 +1,34 @@
-#import sqlite3
-#
-#conn = sqlite3.connect("tree.db")
-#
-#c = conn.cursor()
-#
-#c.execute("""CREATE TABLE tree (
-#    id INTEGER PRIMARY KEY,
-#    first_name TEXT,
-#    last_name TEXT,
-#    family_name TEXT,
-#    born_date DATE,
-#    depth_date INTEGER,
-#    father TEXT,
-#    mother TEXT,
-#    children TEXT,
-#    source_link TEXT
-#)""")
-#
-#conn.commit()
-#
-#conn.close()
+from bardapi import Bard
+from bard_token import token
+import os
+import streamlit as st
+from streamlit_chat import message
+#from streamlit import chat_message as message
+os.environ["_BARD_API_KEY"] = token
 
-#import sqlite3
-#
-#conn = sqlite3.connect("tree.db")
-#
-#c = conn.cursor()
-#
-#c.execute(
-#INSERT INTO tree (first_name, last_name, family_name, born_date, depth_date, father, mother, children, source_link)
-#VALUES ('George', 'H.W.', 'Bush', '1924-06-12', '0', 'Prescott Bush', 'Dorothy Walker Bush',
-#        'Neil Bush', 'George W. Bush', 'https://en.wikipedia.org/wiki/George_H._W._Bush');
-#```)
-#
-#conn.commit()
-#
-#conn.close()
+st.title("Google Bard Clone")
 
-content = """
+def responce_api(promot):
+    message=Bard().get_answer(str(promot))['content']
+    return message
 
-```sql
-INSERT INTO tree (first_name, last_name, family_name, born_date, depth_date, father, mother, children, source_link)
-VALUES ('Tim', 'Kuk', 'Kuk', '2023-08-01', 0, NULL, NULL, NULL, NULL);
-```
+def user_input():
+    input_text = st.text_input("Enter Your Prompt: ")
+    return input_text
 
-This query will insert a new row into the `tree` table with the following values:
-"""
+if 'generate' not in st.session_state:
+    st.session_state['generate']=[]
+if 'past' not in st.session_state:
+    st.session_state['past']=[]
 
+user_text = user_input()
 
-start_pos = content.find("INSERT")
+if user_text:
+    output = responce_api(user_text)
+    st.session_state.generate.append(output)
+    st.session_state.past.append(user_text)
 
-end_pos = content.find(";")
-sql_query = content[start_pos:end_pos + 1].strip()
-print(sql_query)
+if st.session_state['generate']:
+    for i in range(len(st.session_state['generate'])-1, -1, -1):
+        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state['generate'][i], key=str(i))
